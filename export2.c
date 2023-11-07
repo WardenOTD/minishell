16ae8472e7a4ffdfba4dbfb40a646d99ddac7d76
Ivan Laptop <ivan.teohjh@gmail.com>
2023-11-07 15:33:23 +0800

    fixed accepting number as first letter in env name

--- export2.c
+++ export2.c
@@ -6,7 +6,7 @@
 /*   By: jteoh <jteoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:28:29 by jteoh             #+#    #+#             */
-/*   Updated: 2023/11/07 14:06:48 by jteoh            ###   ########.fr       */
+/*   Updated: 2023/11/07 15:32:11 by jteoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ void	export_get(t_lexer *input, t_env *env, t_exp *exp)
 	if (input->arg[2])
 	{
 		printf("export syntax error\n");
+		return ;
+	}
+	if (ft_isdigit(input->arg[1][0]))
+	{
+		printf("Minishell: export: `%s`: not a valid identifier\n", input->arg[1]);
 		return ;
 	}
 	env = add_env(env, input);
