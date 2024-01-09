7f1f931e1ca05114216c2ce0fcb1f25d66681997
jteoh <jteoh@student.42kl.edu.my>
2024-01-09 15:13:32 +0800

    expansion bug fix

--- expansion_flags.c
+++ expansion_flags.c
@@ -6,7 +6,7 @@
 /*   By: jteoh <jteoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 14:30:39 by jteoh             #+#    #+#             */
-/*   Updated: 2024/01/09 14:31:11 by jteoh            ###   ########.fr       */
+/*   Updated: 2024/01/09 15:12:28 by jteoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@ void	get_flag(int *flag, char c, int pos)
 {
 	if (c == '\"')
 	{
+		if (flag[0] % 2 == 0)
+			flag[1] = pos;
 		flag[0] += 1;
-		flag[1] = pos;
 	}
 	else
 	{
-		if (flag[0] % 2 == 0)
-			flag[2] += 1;
-		flag[3] = pos;
+		if (flag[2] % 2 == 0)
+			flag[3] = pos;
+		flag[2] += 1;
 	}
 }
 
